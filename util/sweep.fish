137b229421fc3cc79b250711539225b748b7ee5d
Ronak Buch <ronakbuch@gmail.com>
2022-07-21 02:09:57 -0400

    fixup! Add sweep script

--- util/sweep.fish
+++ util/sweep.fish
@@ -8,23 +8,24 @@ set dir "run-"$pmax"-"$omax"-"$smax"_"(date +%s)
 mkdir $dir
 
 for i in (seq $argv[1])
+    set procs (math 2^$i)
+    echo $procs" procs"
+    mkdir $dir/$procs
     for j in (seq $argv[2])
-	set procs (math 2^$i)
 	set objs (math 10^$j)
 
-	if $objs <= $procs
+	if test $objs -le $procs
 	    continue
 	end
 	
-	set conf $procs"_"$objs
-	echo $conf
-	mkdir $dir/$conf
+	echo " "$objs" objs"
+	mkdir $dir/$procs/$objs
 	for s in (seq 0 4 $argv[3])
 	    for k in (seq 0 3)
 		set seed (math $s + $k)
 		set logfile $procs"_"$objs"_"$seed".log"
-		#echo "./lbsim (math 2^$i) (math 10^$j) $seed & > $dir/$conf/$logfile"
-		./lbsim $procs $objs $seed > $dir/$conf/$logfile &
+		#echo "./lbsim (math 2^$i) (math 10^$j) $seed & > $dir/$procs/$objs/$logfile"
+		./lbsim $procs $objs $seed > $dir/$procs/$objs/$logfile &
 	    end
 	    wait
 	end
